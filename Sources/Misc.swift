768cd876bd938ceb2df3bc84733030034386e517
Stéphane <148355601+auvents-brave@users.noreply.github.com>
2025-08-19 13:53:29 +0200

    Windows

--- Sources/Misc.swift
+++ Sources/Misc.swift
@@ -1,6 +1,12 @@
 import Foundation
 
-public let isRunningTests = ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+public let isRunningTests: Bool = {
+    #if os(Windows)
+        return ProcessInfo.processInfo.environment["SWIFT_TEST_RUNNING"] == "1"
+    #else
+        return ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+    #endif
+}()
 
 public let isRunningInSimulator: Bool = {
     #if targetEnvironment(simulator)
